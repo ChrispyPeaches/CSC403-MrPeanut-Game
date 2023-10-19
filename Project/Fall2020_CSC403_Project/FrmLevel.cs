82af4f386d7d530a18244a71385620834b39f9ce
ChrispyPeaches <gameboy9478@gmail.com>
2023-10-18 21:53:20 -0500

    Minor change

--- Project/Fall2020_CSC403_Project/FrmLevel.cs
+++ Project/Fall2020_CSC403_Project/FrmLevel.cs
@@ -31,7 +31,6 @@ namespace Fall2020_CSC403_Project
 
         private void FrmLevel_Load(object sender, EventArgs e)
         {
-            {
             const int PADDING = 7;
             const int NUM_WALLS = 13;
 
