f2a6980e306c22624c2d58f8a88576ec18605b64
ChrispyPeaches <gameboy9478@gmail.com>
2023-11-13 16:10:41 -0600

    Add hat adjustment for x coordinate & UI for choosing a player

--- Project/Fall2020_CSC403_Project/Cosmetics/CosmeticsShop.Designer.cs
+++ Project/Fall2020_CSC403_Project/Cosmetics/CosmeticsShop.Designer.cs
@@ -38,6 +38,11 @@
             this.btn_AdjustHat_Title = new System.Windows.Forms.Button();
             this.btn_AdjustHatHeight_DownArrow = new System.Windows.Forms.Button();
             this.btn_AdjustHatHeight_UpArrow = new System.Windows.Forms.Button();
+            this.btn_AdjustHatHeight_LeftArrow = new System.Windows.Forms.Button();
+            this.btn_AdjustHatHeight_RightArrow = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_player)).BeginInit();
             this.SuspendLayout();
             // 
@@ -48,9 +53,9 @@
             this.pictureBox_player.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.pictureBox_player.ErrorImage = null;
             this.pictureBox_player.InitialImage = null;
-            this.pictureBox_player.Location = new System.Drawing.Point(12, 208);
+            this.pictureBox_player.Location = new System.Drawing.Point(4, 132);
             this.pictureBox_player.Name = "pictureBox_player";
-            this.pictureBox_player.Size = new System.Drawing.Size(287, 298);
+            this.pictureBox_player.Size = new System.Drawing.Size(275, 323);
             this.pictureBox_player.TabIndex = 1;
             this.pictureBox_player.TabStop = false;
             // 
@@ -61,7 +66,7 @@
             this.btn_ChooseHat_UpArrow.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.btn_ChooseHat_UpArrow.FlatAppearance.BorderSize = 0;
             this.btn_ChooseHat_UpArrow.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_ChooseHat_UpArrow.Location = new System.Drawing.Point(325, 4);
+            this.btn_ChooseHat_UpArrow.Location = new System.Drawing.Point(842, 12);
             this.btn_ChooseHat_UpArrow.Name = "btn_ChooseHat_UpArrow";
             this.btn_ChooseHat_UpArrow.Size = new System.Drawing.Size(65, 61);
             this.btn_ChooseHat_UpArrow.TabIndex = 2;
@@ -75,7 +80,7 @@
             this.btn_ChooseHat_DownArrow.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.btn_ChooseHat_DownArrow.FlatAppearance.BorderSize = 0;
             this.btn_ChooseHat_DownArrow.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_ChooseHat_DownArrow.Location = new System.Drawing.Point(325, 74);
+            this.btn_ChooseHat_DownArrow.Location = new System.Drawing.Point(842, 82);
             this.btn_ChooseHat_DownArrow.Name = "btn_ChooseHat_DownArrow";
             this.btn_ChooseHat_DownArrow.Size = new System.Drawing.Size(65, 61);
             this.btn_ChooseHat_DownArrow.TabIndex = 3;
@@ -90,7 +95,7 @@
             this.btnSelectedHat.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Linen;
             this.btnSelectedHat.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Linen;
             this.btnSelectedHat.ForeColor = System.Drawing.Color.Transparent;
-            this.btnSelectedHat.Location = new System.Drawing.Point(139, 4);
+            this.btnSelectedHat.Location = new System.Drawing.Point(657, 12);
             this.btnSelectedHat.Name = "btnSelectedHat";
             this.btnSelectedHat.Size = new System.Drawing.Size(180, 131);
             this.btnSelectedHat.TabIndex = 0;
@@ -115,10 +120,10 @@
             this.button1.BackColor = System.Drawing.Color.White;
             this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(139, 141);
+            this.button1.Location = new System.Drawing.Point(657, 149);
             this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(180, 47);
+            this.button1.Size = new System.Drawing.Size(250, 47);
             this.button1.TabIndex = 5;
             this.button1.Text = "Choose Hat";
             this.button1.UseVisualStyleBackColor = false;
@@ -127,13 +132,13 @@
             // 
             this.btn_AdjustHat_Title.BackColor = System.Drawing.Color.White;
             this.btn_AdjustHat_Title.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.btn_AdjustHat_Title.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_AdjustHat_Title.Location = new System.Drawing.Point(375, 411);
+            this.btn_AdjustHat_Title.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_AdjustHat_Title.Location = new System.Drawing.Point(526, 63);
             this.btn_AdjustHat_Title.Margin = new System.Windows.Forms.Padding(2);
             this.btn_AdjustHat_Title.Name = "btn_AdjustHat_Title";
-            this.btn_AdjustHat_Title.Size = new System.Drawing.Size(106, 66);
+            this.btn_AdjustHat_Title.Size = new System.Drawing.Size(78, 64);
             this.btn_AdjustHat_Title.TabIndex = 6;
-            this.btn_AdjustHat_Title.Text = "Adjust Hat Height";
+            this.btn_AdjustHat_Title.Text = "Adjust Hat";
             this.btn_AdjustHat_Title.UseVisualStyleBackColor = false;
             // 
             // btn_AdjustHatHeight_DownArrow
@@ -143,9 +148,9 @@
             this.btn_AdjustHatHeight_DownArrow.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.btn_AdjustHatHeight_DownArrow.FlatAppearance.BorderSize = 0;
             this.btn_AdjustHatHeight_DownArrow.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_AdjustHatHeight_DownArrow.Location = new System.Drawing.Point(305, 444);
+            this.btn_AdjustHatHeight_DownArrow.Location = new System.Drawing.Point(537, 132);
             this.btn_AdjustHatHeight_DownArrow.Name = "btn_AdjustHatHeight_DownArrow";
-            this.btn_AdjustHatHeight_DownArrow.Size = new System.Drawing.Size(65, 61);
+            this.btn_AdjustHatHeight_DownArrow.Size = new System.Drawing.Size(51, 43);
             this.btn_AdjustHatHeight_DownArrow.TabIndex = 8;
             this.btn_AdjustHatHeight_DownArrow.UseVisualStyleBackColor = false;
             this.btn_AdjustHatHeight_DownArrow.Click += new System.EventHandler(this.btn_AdjustHatHeight_DownArrow_Click);
@@ -157,12 +162,79 @@
             this.btn_AdjustHatHeight_UpArrow.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.btn_AdjustHatHeight_UpArrow.FlatAppearance.BorderSize = 0;
             this.btn_AdjustHatHeight_UpArrow.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_AdjustHatHeight_UpArrow.Location = new System.Drawing.Point(305, 375);
+            this.btn_AdjustHatHeight_UpArrow.Location = new System.Drawing.Point(537, 16);
             this.btn_AdjustHatHeight_UpArrow.Name = "btn_AdjustHatHeight_UpArrow";
-            this.btn_AdjustHatHeight_UpArrow.Size = new System.Drawing.Size(65, 61);
+            this.btn_AdjustHatHeight_UpArrow.Size = new System.Drawing.Size(51, 42);
             this.btn_AdjustHatHeight_UpArrow.TabIndex = 7;
             this.btn_AdjustHatHeight_UpArrow.UseVisualStyleBackColor = false;
             this.btn_AdjustHatHeight_UpArrow.Click += new System.EventHandler(this.btn_AdjustHatHeight_UpArrow_Click);
+            // 
+            // btn_AdjustHatHeight_LeftArrow
+            // 
+            this.btn_AdjustHatHeight_LeftArrow.BackColor = System.Drawing.Color.Transparent;
+            this.btn_AdjustHatHeight_LeftArrow.BackgroundImage = global::Fall2020_CSC403_Project.Properties.Resources.left_arrow;
+            this.btn_AdjustHatHeight_LeftArrow.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btn_AdjustHatHeight_LeftArrow.FlatAppearance.BorderSize = 0;
+            this.btn_AdjustHatHeight_LeftArrow.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_AdjustHatHeight_LeftArrow.Location = new System.Drawing.Point(483, 71);
+            this.btn_AdjustHatHeight_LeftArrow.Name = "btn_AdjustHatHeight_LeftArrow";
+            this.btn_AdjustHatHeight_LeftArrow.Size = new System.Drawing.Size(38, 50);
+            this.btn_AdjustHatHeight_LeftArrow.TabIndex = 9;
+            this.btn_AdjustHatHeight_LeftArrow.UseVisualStyleBackColor = false;
+            this.btn_AdjustHatHeight_LeftArrow.Click += new System.EventHandler(this.btn_AdjustHatHeight_LeftArrow_Click);
+            // 
+            // btn_AdjustHatHeight_RightArrow
+            // 
+            this.btn_AdjustHatHeight_RightArrow.BackColor = System.Drawing.Color.Transparent;
+            this.btn_AdjustHatHeight_RightArrow.BackgroundImage = global::Fall2020_CSC403_Project.Properties.Resources.right_arrow;
+            this.btn_AdjustHatHeight_RightArrow.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btn_AdjustHatHeight_RightArrow.FlatAppearance.BorderSize = 0;
+            this.btn_AdjustHatHeight_RightArrow.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_AdjustHatHeight_RightArrow.Location = new System.Drawing.Point(610, 71);
+            this.btn_AdjustHatHeight_RightArrow.Name = "btn_AdjustHatHeight_RightArrow";
+            this.btn_AdjustHatHeight_RightArrow.Size = new System.Drawing.Size(36, 50);
+            this.btn_AdjustHatHeight_RightArrow.TabIndex = 10;
+            this.btn_AdjustHatHeight_RightArrow.UseVisualStyleBackColor = false;
+            this.btn_AdjustHatHeight_RightArrow.Click += new System.EventHandler(this.btn_AdjustHatHeight_RightArrow_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.White;
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(4, 460);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(275, 47);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Choose Character";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.BackgroundImage = global::Fall2020_CSC403_Project.Properties.Resources.left_arrow;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(296, 431);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(46, 75);
+            this.button2.TabIndex = 14;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.BackgroundImage = global::Fall2020_CSC403_Project.Properties.Resources.right_arrow;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(361, 431);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(46, 75);
+            this.button3.TabIndex = 15;
+            this.button3.UseVisualStyleBackColor = false;
             // 
             // CosmeticsShop
             // 
@@ -171,8 +243,13 @@
             this.BackgroundImage = global::Fall2020_CSC403_Project.Properties.Resources.shopkeeper_background;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(919, 518);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.btn_AdjustHatHeight_DownArrow);
+            this.Controls.Add(this.btn_AdjustHatHeight_LeftArrow);
             this.Controls.Add(this.btn_AdjustHatHeight_UpArrow);
+            this.Controls.Add(this.btn_AdjustHatHeight_RightArrow);
             this.Controls.Add(this.btn_AdjustHat_Title);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btnExit);
@@ -197,5 +274,10 @@
         private System.Windows.Forms.Button btn_AdjustHat_Title;
         private System.Windows.Forms.Button btn_AdjustHatHeight_DownArrow;
         private System.Windows.Forms.Button btn_AdjustHatHeight_UpArrow;
+        private System.Windows.Forms.Button btn_AdjustHatHeight_LeftArrow;
+        private System.Windows.Forms.Button btn_AdjustHatHeight_RightArrow;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
