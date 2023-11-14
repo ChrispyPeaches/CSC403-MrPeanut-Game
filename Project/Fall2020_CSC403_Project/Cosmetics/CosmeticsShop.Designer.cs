7938f7c9f8a67433f7f641c289b7248e06348ed9
ChrispyPeaches <gameboy9478@gmail.com>
2023-11-14 17:55:00 -0600

    Remove character select arrows

--- Project/Fall2020_CSC403_Project/Cosmetics/CosmeticsShop.Designer.cs
+++ Project/Fall2020_CSC403_Project/Cosmetics/CosmeticsShop.Designer.cs
@@ -41,8 +41,6 @@
             this.btn_AdjustHatHeight_LeftArrow = new System.Windows.Forms.Button();
             this.btn_AdjustHatHeight_RightArrow = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_player)).BeginInit();
             this.SuspendLayout();
             // 
@@ -220,34 +218,6 @@
             this.button4.UseVisualStyleBackColor = false;
             this.button4.Click += new System.EventHandler(this.btnCharacterSelect_Click);
             // 
-            // button2
-            // 
-            this.button2.BackColor = System.Drawing.Color.Transparent;
-            this.button2.BackgroundImage = global::Fall2020_CSC403_Project.Properties.Resources.left_arrow;
-            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.button2.FlatAppearance.BorderSize = 0;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Location = new System.Drawing.Point(395, 530);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(61, 92);
-            this.button2.TabIndex = 14;
-            this.button2.UseVisualStyleBackColor = false;
-            // 
-            // button3
-            // 
-            this.button3.BackColor = System.Drawing.Color.Transparent;
-            this.button3.BackgroundImage = global::Fall2020_CSC403_Project.Properties.Resources.right_arrow;
-            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.button3.FlatAppearance.BorderSize = 0;
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.Location = new System.Drawing.Point(481, 530);
-            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(61, 92);
-            this.button3.TabIndex = 15;
-            this.button3.UseVisualStyleBackColor = false;
-            // 
             // CosmeticsShop
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -255,8 +225,6 @@
             this.BackgroundImage = global::Fall2020_CSC403_Project.Properties.Resources.shopkeeper_background;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1225, 638);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.btn_AdjustHatHeight_DownArrow);
             this.Controls.Add(this.btn_AdjustHatHeight_LeftArrow);
@@ -290,7 +258,5 @@
         private System.Windows.Forms.Button btn_AdjustHatHeight_LeftArrow;
         private System.Windows.Forms.Button btn_AdjustHatHeight_RightArrow;
         private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
     }
 }
